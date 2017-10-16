5bccb8c780acfeabecb4bf2ee2cb7df4bc77ee6b
mskalbania <cod634.steam@gmail.com>
2017-10-16 17:10:52 +0200

    quick sort

---
+++ SortingAlgorithms/src/main/java/com/sortingalgorithms/QuickSort.java
@@ -0,0 +1,10 @@
+package com.sortingalgorithms;
+
+import java.util.List;
+
+public class QuickSort implements SortingAlgorithm{
+    @Override
+    public void sort(List<Integer> array) {
+
+    }
+}
